84a8879f79f478d12c77fc8cbed4622405161593
Fira <colour.shade.comics@gmail.com>
2026-02-21 13:15:53 +0300

    finished

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ function rgbToHex(r, g, b) {
 //Задача 2
 let count = document.querySelector("[data-js='t2-count']")
 let input = document.querySelector("[data-js='t2-input']")
-input.addEventListener('keydown',() => {
+document.addEventListener('input',() => {
   count.textContent = input.value.length
 })
 
@@ -106,19 +106,28 @@ let pane_b = document.querySelector("[data-pane='b'")
 let pane_c = document.querySelector("[data-pane='c'")
 
 tab_a.addEventListener('click', () => {
-  pane_a.classList.replace('pane', 'pane is-active')
-  pane_b.classList.replace('pane is-active', 'pane')
-  pane_c.classList.replace('pane is-active', 'pane')
+  pane_a.classList.add('is-active')
+  pane_b.classList.remove('is-active')
+  pane_c.classList.remove('is-active')
+  tab_a.classList.add('is-active')
+  tab_b.classList.remove('is-active')
+  tab_c.classList.remove('is-active')
 })
 tab_b.addEventListener('click', () => {
-  pane_b.classList.replace('pane', 'pane is-active')
-  pane_a.classList.replace('pane is-active', 'pane')
-  pane_c.classList.replace('pane is-active', 'pane')
+  pane_b.classList.add('is-active')
+  pane_a.classList.remove('is-active')
+  pane_c.classList.remove('is-active')
+  tab_b.classList.add('is-active')
+  tab_a.classList.remove('is-active')
+  tab_c.classList.remove('is-active')
 })
-tab_a.addEventListener('click', () => {
-  pane_c.classList.replace('pane', 'pane is-active')
-  pane_b.classList.replace('pane is-active', 'pane')
-  pane_a.classList.replace('pane is-active', 'pane')
+tab_c.addEventListener('click', () => {
+  pane_c.classList.add('is-active')
+  pane_b.classList.remove('is-active')
+  pane_a.classList.remove('is-active')
+  tab_c.classList.add('is-active')
+  tab_b.classList.remove('is-active')
+  tab_a.classList.remove('is-active')
 })
 
 
@@ -160,50 +169,147 @@ let text = document.querySelector("[data-js='t9-text']")
 let btn9 = document.querySelector("[data-js='t9-toggle']")
 
 btn9.addEventListener('click', () => {
-  if (btn9.textContent == "Скрыть"){
-    text.setAttribute('hidden', '')
+  if (text.hidden == false){
+    text.hidden = true
     btn9.textContent = "Показать"
   }
   else{
-    text.removeAttribute('hidden')
+    text.hidden = false
     btn9.textContent = "Скрыть"
   }
 
 })
 
 // Задача 10
-let square = document.querySelector("[data-js='t10-box']")
-let size = document.querySelector("[data-js='t10-out']")
+let box10 = document.querySelector("[data-js='t10-box']")
+let out10 = document.querySelector("[data-js='t10-out']")
 let range = document.querySelector("[data-js='t10-range']")
+let size
 
+  range.addEventListener('input', () => {
+    size = range.value
+    box10.style.width = size + 'px';
+    box10.style.height = size + 'px';
+    out10.textContent = size + 'px';
 
+})
 
 
 // Задача 11
 let input11 = document.querySelector("[data-js='t11-seconds']")
 let start = document.querySelector("[data-js='t11-start']")
-let stop = document.querySelector("[data-js='t11-stop']")
+let stop11 = document.querySelector("[data-js='t11-stop']")
 let output11 = document.querySelector("[data-js='t11-out']")
-let time_left = 0
+let timeLeft = 0
+let timer = 0
 
 start.addEventListener('click', () => {
-    timer_left = Number(input11.value)
-    console.log("it works")
-    input11.value = ''
-    let timer = setInterval(() => {
-      output11.textContent = time_left
-      if (time_left <= 0) {
-        clearInterval(timer)
-        output11.textContent = "Время вышло!"
-      } 
-      else {
-        time_left--
-      }
-    }, 1000)
-
+  clearInterval(timer)
+  timeLeft = parseInt(input11.value)
+  input11.value = ""
+  let updateTimer = () => {
+    output11.textContent = timeLeft
+    if (timeLeft <= 0) {
+      clearInterval(timer)
+      output11.textContent = "Время вышло!"
+      return;
+    }
+    timeLeft--;
+  }
+  updateTimer()
+  timer = setInterval(updateTimer, 1000)
 })
 
-stop.addEventListener('click', () => {
+stop11.addEventListener('click', () => {
   clearInterval(timer)
-  output11.textContent = "Таймер остановлен!"
+})
+
+
+// Задача 12
+let btn12 = document.querySelector("[data-js='t12-plus']")
+let out12 = document.querySelector("[data-js='t12-label']")
+let bar = document.querySelector("[data-js='t12-bar']")
+let barWidth = 0
+
+btn12.addEventListener('click', () => {
+  if (barWidth < 100){
+    barWidth += 10
+    bar.style.width = String(barWidth) + "%"
+  }
+  out12.textContent = barWidth + "%"
+})
+
+// Задача 13
+let input13 = document.querySelector("[data-js='t13-input']")
+let out13 = document.querySelector("[data-js='t13-out']")
+
+out13.textContent = "Нажмите Ctrl+K для очистки ввода"
+
+document.addEventListener('keydown', function(e){
+  if (e.ctrlKey && e.key === "k") {
+    e.preventDefault();
+    input13.value = ""
+    out13.textContent = "Ctrl+K нажат! Ввод очищен"
+  }
+})
+
+// Задача 14
+let btn14 = document.querySelector("[data-js='t14-next']")
+let out14 = document.querySelector("[data-js='t14-out']")
+const arr = ["Волк слишком энергичен чтобы работать",
+  "Упал – не значит упал. Провал – это там где не встал",
+  "Лучше быть тем кем есть, чем быть тем, кем не будешь",
+  "Если волк молчит, то лучше его не перебивать"
+]
+
+btn14.addEventListener('click', () => {
+  const random = Math.floor(Math.random() * arr.length)
+  out14.textContent = arr[random]
+})
+
+
+// Задача 15
+let coords = document.querySelector("[data-js='t15-coords']")
+let area = document.querySelector("[data-js='t15-area']")
+let item = document.querySelector("[data-js='t15-item']")
+let dragging = false
+let x, y
+
+item.addEventListener('mousedown', function(e){
+  x = e.x - item.getBoundingClientRect().left
+  y = e.y - item.getBoundingClientRect().top
+  area.append(item)
+  dragging = true
+})
+
+document.addEventListener('mousemove',function(e){
+  if (dragging == false) return
+  const areaRect = area.getBoundingClientRect()
+  const itemBox = item.getBoundingClientRect()
+  const itemWidth = itemBox.width
+  const itemHeight = itemBox.height
+
+  let newLeft = e.x - area.getBoundingClientRect().left - x
+  let newTop = e.y - area.getBoundingClientRect().top - y
+
+  newLeft = Math.max(0, Math.min(newLeft, areaRect.width - itemWidth))
+  newTop = Math.max(0, Math.min(newTop, areaRect.height - itemHeight))
+  
+  item.style.left = newLeft + 'px'
+  item.style.top = newTop + 'px'
+  
+  coords.textContent = `x: ${Math.round(newLeft)}, y: ${Math.round(areaRect.height - itemHeight - newTop)}`
+})
+
+document.addEventListener('mouseup', () => {
+  dragging = false
+})
+
+  
+// Задача 16
+let btn16 = document.querySelector("[data-js='t16-animate']")
+let box16 = document.querySelector("[data-js='t16-box']")
+
+btn16.addEventListener('click', () => {
+  box16.classList.toggle('is-bouncing')
 })
